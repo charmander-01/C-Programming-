d4778fe30ca830fa8158229fa5029de51af95bf4
Luís Miguel <92678690+charmander-01@users.noreply.github.com>
2022-10-18 07:56:45 -0300

    Update q2.c

--- AED2/Revisão/q2.c
+++ AED2/Revisão/q2.c
@@ -1,1 +1,39 @@
+#include <stdio.h>
 
+int main()
+{
+    int i, j;
+    int valor;
+    int inicio, fim;
+    int div;
+
+    scanf("%d", &valor);
+
+    int vet[valor];
+    int index = 0;
+
+    scanf("%d %d", &inicio, &fim);
+
+    for(i=2;i<=valor;i++)
+    {
+        div = 0;
+        for(j=1;j<=i;j++)
+        {
+            if(i%j==0) div++;
+        }
+        if(div==2)
+        {
+            vet[index] = i;
+            index++;
+        }
+    }
+
+    for(i=inicio-1;i<=fim-1;i++)
+    {
+        printf("%d ", vet[i]);
+    }    
+
+    printf("\n");
+
+    return 0;
+}
